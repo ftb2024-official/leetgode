fafe8ae0a08ace42059cc3b6e61d0b5f5c970f3d
ftb2024_official <ftb2024@gmail.com>
2024-03-03 17:25:29 +0500

    five more solutions

--- arrays_and_hashing/167-two-sum-II-input-array-is-sorted.go
+++ arrays_and_hashing/167-two-sum-II-input-array-is-sorted.go
@@ -1,1 +1,51 @@
+/*
+Given a 1-indexed array of integers `numbers` that is already sorted in ascending order, find two numbers such that they add up to specific
+`target` number. Let these two numbers be `numbers[idx1]` and `numbers[idx2]`, where `1 <= idx1 < idx2 <= numbers.length`. Return the indices of
+the two numbers `idx1` and `idx2`, added by one as an integer array `[idx1, idx2]` of length 2. The tests are generated such that there is exacyly
+one solution. You may not use the same element twice. Your solution must use only constant extra space.
+
+Example 1:
+	Input: numbers = [2,7,11,15], target = 9
+	Output: [1,2]
+	Explanation: The sum of 2 and 7 is 9. Therefore, index1 = 1, index2 = 2. We return [1, 2].
+
+Example 2:
+	Input: numbers = [2,3,4], target = 6
+	Output: [1,3]
+	Explanation: The sum of 2 and 4 is 6. Therefore index1 = 1, index2 = 3. We return [1, 3].
+
+Example 3:
+	Input: numbers = [-1,0], target = -1
+	Output: [1,2]
+	Explanation: The sum of -1 and 0 is -1. Therefore index1 = 1, index2 = 2. We return [1, 2].
+*/
+
 package arraysandhashing
+
+func TwoSumII(numbers []int, target int) []int {
+	result := []int{}
+
+	for i := 0; i < len(numbers)-1; i++ {
+		if i > 0 && numbers[i] == numbers[i-1] {
+			continue
+		}
+
+		right := len(numbers) - 1
+
+		for i < right {
+			twoSum := numbers[i] + numbers[right]
+			if twoSum > target {
+				right--
+			} else if twoSum < target {
+				i++
+			} else {
+				result = append(result, i+1, right+1)
+
+				i++
+				right--
+			}
+		}
+	}
+
+	return result
+}
--- arrays_and_hashing/18-four-sum.go
+++ arrays_and_hashing/18-four-sum.go
@@ -1,1 +1,65 @@
+/*
+Given an array `nums` of `n` integers, return an array of all the unique quadruplets `[nums[a], nums[b], nums[c], nums[d]]` such that
+`0 <= a, b, c, d < n`, `a, b, c, d are distinct`, and `nums[a] + nums[b] + nums[c] + nums[d] = target`. You may return the answer in any order.
+
+Example 1:
+	Input: nums = [1,0,-1,0,-2,2], target = 0
+	Output: [[-2,-1,1,2],[-2,0,0,2],[-1,0,0,1]]
+
+Example 2:
+	Input: nums = [2,2,2,2,2], target = 8
+	Output: [[2,2,2,2]]
+*/
+
 package arraysandhashing
+
+import (
+	"slices"
+)
+
+func FourSum(nums []int, target int) [][]int {
+	if len(nums) < 4 {
+		return nil
+	}
+
+	result := [][]int{}
+	slices.Sort(nums)
+
+	for i := 0; i < len(nums)-3; i++ {
+		if i > 0 && nums[i] == nums[i-1] {
+			continue
+		}
+
+		for j := i + 1; j < len(nums)-2; j++ {
+			if j > i+1 && nums[j] == nums[j-1] {
+				continue
+			}
+
+			left, right := j+1, len(nums)-1
+
+			for left < right {
+				fourSum := nums[i] + nums[j] + nums[left] + nums[right]
+				if fourSum > target {
+					right--
+				} else if fourSum < target {
+					left++
+				} else {
+					result = append(result, []int{nums[i], nums[j], nums[left], nums[right]})
+
+					for left < right && nums[left] == nums[left+1] {
+						left++
+					}
+
+					for left < right && nums[right] == nums[right-1] {
+						right--
+					}
+
+					left++
+					right--
+				}
+			}
+		}
+	}
+
+	return result
+}
