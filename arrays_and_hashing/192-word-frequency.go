481048b398d1cf945f973ae85a5958ec94d4cfda
ftb2024_official <ftb2024@gmail.com>
2024-03-27 09:01:43 +0500

    leetgode 192 done

--- arrays_and_hashing/192-word-frequency.go
+++ arrays_and_hashing/192-word-frequency.go
@@ -1,1 +1,45 @@
+/*
+Write script to calculate the frequency of each word in a text file `words.txt`. For simplicity sake you may assume: 1) `words.txt` contains only
+lowercase characters and space characters; 2) Each word must consist of lowercase characters only; 3) Words are separated by one or more whitespace
+characters.
+
+Example:
+	Input: the day is sunny the the\nthe sunny is is
+	Output:
+		the 4
+		is 3
+		sunny 2
+		day 1
+*/
+
 package arraysandhashing
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func CountWordFrequency() map[string]int {
+	if len(os.Args) < 2 {
+		fmt.Println("Usage: go run main.go <filename>")
+		return nil
+	}
+
+	file, err := os.Open(os.Args[1])
+	if err != nil {
+		fmt.Println("Error:", err)
+		return nil
+	}
+	defer file.Close()
+
+	scanner := bufio.NewScanner(file)
+	ans := map[string]int{}
+
+	scanner.Split(bufio.ScanWords)
+	for scanner.Scan() {
+		ans[scanner.Text()]++
+	}
+
+	return ans
+}
